5c31bc64f095194af77ae37d16487d86bb203cfd
Jake <forte2610@hotmail.com>
2018-04-09 18:18:10 +0700

    Added security config

---
+++ src/main/java/com/titan/probe/config/SecurityConfig.java
@@ -0,0 +1,11 @@
+package com.titan.probe.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
