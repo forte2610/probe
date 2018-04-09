c5559d43720d80ff41fdf43127a09066ac8c8328
Jake <forte2610@hotmail.com>
2018-04-09 18:02:17 +0700

    Fixed UserRepository bean conflict

--- src/main/java/com/titan/probe/services/UserServiceImplementation.java
+++ src/main/java/com/titan/probe/services/UserServiceImplementation.java
@@ -3,12 +3,14 @@ package com.titan.probe.services;
 import com.titan.probe.models.User;
 import com.titan.probe.Repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service("userService")
 public class UserServiceImplementation implements UserService {
     @Autowired
+    @Qualifier("userRepository")
     private UserRepository userRepository;
     @Autowired
     private BCryptPasswordEncoder bCryptPasswordEncoder;
