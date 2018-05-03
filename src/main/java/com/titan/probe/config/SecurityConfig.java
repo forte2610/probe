073b59a57426a5456918583f089fe7eba084d379
Jake <forte2610@hotmail.com>
2018-05-04 02:13:48 +0700

    Updated favicon

--- src/main/java/com/titan/probe/config/SecurityConfig.java
+++ src/main/java/com/titan/probe/config/SecurityConfig.java
@@ -50,6 +50,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/search").permitAll()
                 .antMatchers("/vendors").permitAll()
                 .antMatchers("/vendor-details/**").permitAll()
+                .antMatchers("/favicon.ico").permitAll()
                 .anyRequest()
                 .authenticated().and().csrf().disable().formLogin()
                 .loginPage("/login").failureUrl("/login?error=true")
